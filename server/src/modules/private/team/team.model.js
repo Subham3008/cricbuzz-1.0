7de4c7bdffba6bd9cf0931283bbc994f59612fef
Subham3008 <subhamsamanta065@gmail.com>
2026-06-13 01:43:11 +0530

    model: create team model

---
+++ server/src/modules/private/team/team.model.js
@@ -0,0 +1,19 @@
+import mongoose, { Schema, model } from "mongoose"
+
+const teamSchema = new Schema(
+  {
+    name: { type: String, required: true, trim: true, unique: true },
+    shortName: { type: String, required: true, trim: true, unique: true },
+    logo: { type: String, required: true },
+    primaryColor: String,
+    squadPlayers: [{ type: Schema.Types.ObjectId, ref: "Player" }],
+    isDeleted: { type: Boolean, default: false },
+    createdBy: { type: Schema.Types.ObjectId, ref: "User" },
+    updatedBy: { type: Schema.Types.ObjectId, ref: "User" },
+  },
+  { timestamps: true }
+);
+
+const Team = model("Team", teamSchema)
+
+export default Team
