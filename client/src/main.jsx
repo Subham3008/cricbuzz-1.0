65c98cb50faa0c9a2eff29f288435912870e7e63
Siddhant Mul <siddhantmul18@gmail.com>
2026-06-16 10:24:49 +0530

    resolve main.jsx conflict

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,12 @@
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import { Provider } from "react-redux";
-import store from "./app/store/store.js";
-import AppRoutes from "./app/routes/AppRoutes.jsx";
-
-createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <AppRoutes />
-  </Provider>,
-);
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { Provider } from "react-redux"
+import store from "./app/store/store.js"
+import AppRoutes from './app/routes/AppRoutes.jsx'
+createRoot(document.getElementById('root')).render(
+ <Provider store={store}>
+<AppRoutes/>
+ </Provider>
+)
